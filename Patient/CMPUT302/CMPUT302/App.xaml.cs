230e67430e6c08cd9c43d58870cdebfd93f58855
Jeremy Unger <jmunger@ualberta.ca>
2013-04-06 18:48:51 -0600

    Added App.cs fields

--- Patient/CMPUT302/CMPUT302/App.xaml.cs
+++ Patient/CMPUT302/CMPUT302/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using Microsoft.Kinect;
 
 namespace CMPUT302
 {
@@ -12,7 +13,12 @@ namespace CMPUT302
     /// </summary>
     public partial class App : Application
     {
-        public static Axis xaxis, yaxis;
-        public static Point zero, xMax, yMax;
+        public static String joint = "Left";
+        public readonly static int XPIXEL = 1366;
+        public readonly static int YPIXEL = 768;
+        public static Axis xaxis = new Axis();
+        public static Axis yaxis = new Axis();
+        public static Skeleton mainSkeleton = new Skeleton();
+        public static bool donecal;
     }
 }
